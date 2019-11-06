bac652843da49357bffa32928d74c730fb1b5c12
Pier-Olivier Fontaine <thepiero294@gmail.com>
2019-11-06 15:51:01 -0500

    Créer tableau attaque, mouseEnter et statut bateau HTML

--- Battleship/js/main.js
+++ Battleship/js/main.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-  function creationGrilleDef()
-  {
+  function creationGrilleDef() {
     const grille = document.getElementById('grilleDef');
     const grilleDef = document.createElement('table');
 
     // Création des chiffres pour les colonnes
     for (let i = 0; i < 1; i++) {
-      var tr = document.createElement('tr');
+      const tr = document.createElement('tr');
 
       for (let j = 0; j < 11; j++) {
-        var th = document.createElement('th');
+        const th = document.createElement('th');
         $(th).addClass('title-col');
 
         if (j > 0) {
@@ -20,14 +19,16 @@ $(document).ready(function() {
       grilleDef.appendChild(tr);
     }
 
+    // Création des cases de la grille de défense
     for (let i = 0; i < 10; i++) {
-      var tr = document.createElement('tr');
+      const tr = document.createElement('tr');
 
       $(tr).addClass('titre-lig');
-      $(tr).append(String.fromCharCode(65 + i))
+      $(tr).append(String.fromCharCode(65 + i));
+      const lettre = String.fromCharCode(65 + i);
       for (let j = 0; j < 10; j++) {
-        var td = document.createElement('td');
-        $(td).addClass('eau');
+        const td = document.createElement('td');
+        $(td).addClass('eau').attr('id', lettre + '-' + (j + 1));
         tr.appendChild(td);
       }
       grilleDef.appendChild(tr);
@@ -35,5 +36,53 @@ $(document).ready(function() {
     grille.appendChild(grilleDef);
   }
 
+  function creationGrilleAtt() {
+    const grille = document.getElementById('grilleAtt');
+    const grilleAtt = document.createElement('table');
+
+    // Création des chiffres pour les colonnes
+    for (let i = 0; i < 1; i++) {
+      const tr = document.createElement('tr');
+
+      for (let j = 0; j < 11; j++) {
+        const th = document.createElement('th');
+        $(th).addClass('title-col');
+
+        if (j > 0) {
+          th.append(j);
+        }
+        tr.appendChild(th);
+      }
+      grilleAtt.appendChild(tr);
+    }
+
+    // Création des cases de la grille attaque
+    for (let i = 0; i < 10; i++) {
+      const tr = document.createElement('tr');
+
+      $(tr).addClass('titre-lig');
+      $(tr).append(String.fromCharCode(65 + i));
+      const lettre = String.fromCharCode(65 + i);
+      for (let j = 0; j < 10; j++) {
+        const td = document.createElement('td');
+        $(td).addClass('eau').attr('id', lettre + '-' + (j + 1));
+        tr.appendChild(td);
+      }
+      grilleAtt.appendChild(tr);
+    }
+    grille.appendChild(grilleAtt);
+  }
+
+  function caseEnterCouleur() {
+    $('.eau').mouseenter(function() {
+      $(this).css('opacity', '0.5');
+    });
+    $('.eau').mouseleave(function() {
+      $(this).css('opacity', '1');
+    });
+  }
+
   creationGrilleDef();
+  creationGrilleAtt();
+  caseEnterCouleur();
 });
